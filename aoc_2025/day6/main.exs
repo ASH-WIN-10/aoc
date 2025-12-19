273e8d70cb9caf4d800ff3a96ceba491c3212a7e
Ashvin Jangid <142579833+ASH-WIN-10@users.noreply.github.com>
2025-12-20 01:28:08 +0530

    better 2025: day6

--- aoc_2025/day6/main.exs
+++ aoc_2025/day6/main.exs
@@ -1,16 +1,27 @@
 defmodule Solution do
-  def part1(nums, operators) do
-    len = length(operators)
-
-    Enum.reduce(0..(len - 1), 0, fn i, acc ->
-      column = Enum.map(nums, fn row -> Enum.at(row, i) end)
-      operator = Enum.at(operators, i)
-
+  def calculate(nums, operators) do
+    Enum.zip(nums, operators)
+    |> Enum.reduce(0, fn {group, op}, acc ->
       acc +
-        if operator == "+",
-          do: Enum.sum(column),
-          else: Enum.product(column)
+        case op do
+          "+" -> Enum.sum(group)
+          _ -> Enum.product(group)
+        end
     end)
+  end
+
+  def part1(contents) do
+    {rows, [operators]} =
+      contents
+      |> Enum.map(&String.split/1)
+      |> Enum.split(-1)
+
+    nums =
+      Enum.map(rows, &Enum.map(&1, fn x -> String.to_integer(x) end))
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+
+    calculate(nums, operators)
   end
 
   def extract_nums(contents) do
@@ -25,27 +36,15 @@ defmodule Solution do
       if column == "", do: column <> "  " <> str, else: column <> " " <> str
     end)
     |> String.split("  ")
-    |> Enum.map(fn x ->
-      String.split(x) |> Enum.map(&String.to_integer/1)
-    end)
+    |> Enum.map(fn x -> String.split(x) |> Enum.map(&String.to_integer/1) end)
   end
 
   def part2(contents) do
-    {nums_str, operators} = Enum.split(contents, -1)
-    [operators] = Enum.map(operators, &String.split/1)
-    len = length(operators)
-
+    {nums_str, [operators]} = Enum.split(contents, -1)
+    operators = String.split(operators)
     nums = extract_nums(nums_str)
 
-    Enum.reduce(0..(len - 1), 0, fn i, acc ->
-      column = Enum.at(nums, i)
-      operator = Enum.at(operators, i)
-
-      acc +
-        if operator == "+",
-          do: Enum.sum(column),
-          else: Enum.product(column)
-    end)
+    calculate(nums, operators)
   end
 end
 
@@ -53,13 +52,5 @@ contents =
   File.read!("day6/input.txt")
   |> String.split("\n", trim: true)
 
-{nums, [operators]} =
-  contents
-  |> Enum.map(&String.split/1)
-  |> Enum.split(-1)
-  |> then(fn {first, last} ->
-    {Enum.map(first, &Enum.map(&1, fn x -> String.to_integer(x) end)), last}
-  end)
-
-IO.puts("Part 1: #{Solution.part1(nums, operators)}")
+IO.puts("Part 1: #{Solution.part1(contents)}")
 IO.puts("Part 2: #{Solution.part2(contents)}")
